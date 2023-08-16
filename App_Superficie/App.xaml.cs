59ce4825c1c07888418422c6b2d51e6566cedea7
28904 <28904@KNOBBLEBOY>
2023-08-16 05:54:09 -0600

    Agregar archivos de proyecto.

---
+++ App_Superficie/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace App_Superficie;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ App_Superficie/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace App_Superficie;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Calcular_Clicked(object sender, EventArgs e)
+    {
+		int Base1, Altura1, Area;
+
+		Base1 = Convert.ToInt32(Base.Text);
+		Altura1 = Convert.ToInt32(Altura.Text);
+
+		Area = (Base1 * Altura1) / 2;
+
+		LblArea.Text = "El resultado es :" + Area + "cm^2";
+    }
+}
+
